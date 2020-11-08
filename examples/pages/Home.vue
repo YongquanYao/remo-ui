20c1872ebde3091f38f22f2968eacc9c1983033c
Raymond <rrraymond810@gmail.com>
2020-11-08 17:08:49 -0500

    feat new example homePage layout

--- examples/pages/Home.vue
+++ examples/pages/Home.vue
@@ -2,12 +2,32 @@
     <div class="home">
         <div class="header">
             <div class="container">
-                Remo UI
+                <h2>Remo UI</h2>
             </div>
         </div>
         <div class="main">
-            <div class="sidebar"></div>
+            <div class="sidebar">
+                <ul>
+                    <li class="nav-item" :key="x.title" v-for="x in sidebar_data">
+                        <a>{{x.title}}</a>
+                        <ul class="remo-menu-list" v-if="x.child_data.length !== 0">
+                            <li class="nav-item" :key="y.title" v-for="y in x.child_data">
+                              <div class="nav-group-title" v-if="y.group">{{y.group}}</div>
+                              <!-- 这里涉及active 和 inactive的选项 -->
+                              <a
+                                :class="{'active': active == y.id}"
+                                @click="active=y.id"
+                                :href="y.path"
+                              >
+                                {{y.title}}
+                              </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
             <div class="content">
+                <router-view></router-view>
             </div>
         </div>
         <div class="footer"></div>
@@ -16,6 +36,93 @@
 
 <script>
 export default {
+  name: 'Home',
+  data () {
+    return {
+      active: 1,
+      sidebar_data: [
+        {
+          title: '开发指南',
+          child_data: [
+            {
+              title: '安装/快速上手'
+            },
+            {
+              title: 'Icon 图标'
+            },
+            {
+              title: 'About 关于'
+            }
+          ]
+        },
+        {
+          title: '组件',
+          child_data: [
+            {
+              group: '通用',
+              title: 'Button 按钮'
+            },
+            {
+              title: 'Shadow Block 阴影模块'
+            },
+            {
+              group: '数据录入',
+              title: 'Input 输入框'
+            },
+            {
+              title: 'Upload 上传'
+            }
+          ]
+        },
+        {
+          title: '组件',
+          child_data: [
+            {
+              group: '通用',
+              title: 'Button 按钮'
+            },
+            {
+              title: 'Shadow Block 阴影模块'
+            },
+            {
+              group: '数据录入',
+              title: 'Input 输入框'
+            },
+            {
+              title: 'Upload 上传'
+            }
+          ]
+        },
+        {
+          title: '组件',
+          child_data: [
+            {
+              group: '通用',
+              title: 'Button 按钮'
+            },
+            {
+              title: 'Shadow Block 阴影模块'
+            },
+            {
+              group: '数据录入',
+              title: 'Input 输入框'
+            },
+            {
+              title: 'Upload 上传'
+            }
+          ]
+        },
+        {
+          title: '实用插件',
+          child_data: [
+            {
+              title: '开发ing...'
+            }
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -57,16 +164,62 @@ export default {
             width: 200px;
             height: 80%;
             top: 135px;
-            visibility: hidden; // 鼠标在才触发滚动条
+            // visibility: hidden; // 鼠标在才触发滚动条
+            border-right: 1px solid #dcdfe6; // 分界线
             overflow: auto;
             &:hover{
                 visibility: visible; // 鼠标在才触发滚动条
+                border-right: 1px solid #dcdfe6; // 分界线
             }
             > ul{
                 visibility: visible; // 始终显示列表
+                padding: 0;
             }
             .nav-item{
-
+              list-style: none;
+              a{
+                font-size: 16px;
+                color: #333;
+                line-height: 40px;
+                height: 40px;
+                margin: 0;
+                padding: 0;
+                text-decoration: none;
+                display: block;
+                position: relative;
+                //active点击时变颜色
+                transition: 0.15s ease-out;
+                font-weight: 700;
+                cursor: unset;
+              }
+              .nav-group-title{
+                font-size: 12px;
+                color: #999;
+                line-height: 26px;
+                margin-top: 15px;
+                font-weight: 400;
+              }
+            }
+            .remo-menu-list{
+              padding: 0;
+              a{
+                display: block;
+                height: 40px;
+                line-height: 40px;
+                color: #555;
+                font-size: 14px;
+                overflow: hidden;
+                white-space: nowrap;
+                text-overflow: ellipsis;
+                font-weight: 500;
+                cursor: pointer;
+                &:hover{
+                  color:#409eff
+                }
+              }
+              .active{
+                  color:#409eff
+              }
             }
         }
         .content {
