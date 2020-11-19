9900fcbece50d330cada8d23a53125d4b7692baa
Raymond <rrraymond810@gmail.com>
2020-11-19 12:31:38 -0500

    feat remo-img-card / new demo

--- examples/components/remo-imgcard-demo.vue
+++ examples/components/remo-imgcard-demo.vue
@@ -2,56 +2,107 @@
     <div>
         <demo-block header="Image Card 图卡">
         </demo-block>
-        <demo-block title="基础" desc="展示如下：">
-            <re-tip color="blue">敬请期待,开发ing....</re-tip>
+        <demo-block title="基础" desc="静态展示：">
+            <!-- <re-tip color="blue">敬请期待,开发ing....</re-tip> -->
+             <re-img-card one="123">
+                <template v-slot:img>
+                    <img src="../assets/4.jpg" alt="">
+                </template>
+                <template slot="desc">
+                    <p style="margin-top: 30px">This is description Powered by Remo UI. </p>
+                </template>
+            </re-img-card>
+            <re-img-card >
+                <template v-slot:img>
+                    <img src="../assets/5.jpg" alt="">
+                </template>
+                <template slot="desc">
+                    <p style="margin-top: 30px">This is description Powered by Remo UI. </p>
+                </template>
+            </re-img-card>
+            <re-img-card >
+                <template v-slot:img>
+                    <img src="../assets/6.jpg" alt="">
+                </template>
+                <template slot="desc">
+                    <p style="margin-top: 30px">This is description Powered by Remo UI. </p>
+                </template>
+            </re-img-card>
         </demo-block>
-        <re-img-card >
-            <template slot="img">
-                <img src="../assets/1.jpg"  alt="">
-            </template>
-            <template slot="desc">
-                <p style="margin-top: 30px">This is description Powered by Remo UI </p>
-            </template>
-        </re-img-card>
-        <re-img-card >
-            <template v-slot:img>
-                <img src="../assets/2.jpg"  alt="">
-            </template>
-            <template slot="desc">
-                <p style="margin-top: 30px">This is description Powered by Remo UI </p>
-            </template>
-        </re-img-card>
-        <re-img-card >
-            <template v-slot:img>
-                <img src="../assets/3.jpg" alt="">
-            </template>
-            <template slot="desc">
-                <p style="margin-top: 30px">This is description Powered by Remo UI </p>
-            </template>
-        </re-img-card>
-        <re-img-card >
-            <template v-slot:img>
-                <img src="../assets/4.jpg" alt="">
-            </template>
-            <template slot="desc">
-                <p style="margin-top: 30px">This is description Powered by Remo UI </p>
-            </template>
-        </re-img-card>
-        <re-img-card >
-            <template v-slot:img>
-                <img src="../assets/5.jpg" alt="">
-            </template>
-            <template slot="desc">
-                <p style="margin-top: 30px">This is description Powered by Remo UI </p>
-            </template>
-        </re-img-card><re-img-card >
-            <template v-slot:img>
-                <img src="../assets/6.jpg" alt="">
-            </template>
-            <template slot="desc">
-                <p style="margin-top: 30px">This is description Powered by Remo UI </p>
-            </template>
-        </re-img-card>
+        <demo-block title="动态显示" desc="设置type为dynamic：">
+             <re-img-card  type="dynamic">
+                <template slot="img">
+                    <img src="../assets/1.jpg"  alt="">
+                </template>
+                <template slot="desc">
+                    <p style="margin-top: 30px">Dynamic image card Powered by Remo UI. </p>
+                </template>
+            </re-img-card>
+            <re-img-card type="dynamic" >
+                <template v-slot:img>
+                    <img src="../assets/2.jpg"  alt="">
+                </template>
+                <template slot="desc">
+                    <p style="margin-top: 30px">Dynamic image card Powered by Remo UI. </p>
+                </template>
+            </re-img-card>
+            <re-img-card type="dynamic">
+                <template v-slot:img>
+                    <img src="../assets/3.jpg" alt="">
+                </template>
+                <template slot="desc">
+                    <p style="margin-top: 30px">Dynamic image card Powered by Remo UI. </p>
+                </template>
+            </re-img-card>
+        </demo-block>
+         <demo-block title="自定义" desc="可设置图片高度与右上角icon图标：">
+             <re-img-card feature="off">
+                <template slot="img">
+                    <img src="../assets/8.jpg"  alt="">
+                </template>
+                <template slot="desc">
+                    <p style="margin-top: 30px"> Image card without right top feature. </p>
+                </template>
+            </re-img-card>
+            <re-img-card type="static" iconTop="remo-zoomin" iconBottom="remo-delete" >
+                <template v-slot:img>
+                    <img src="../assets/8.jpg"  alt="">
+                </template>
+                <template slot="desc">
+                    <p style="margin-top: 30px"> Personal setting right top feature.</p>
+                </template>
+            </re-img-card>
+            <div style='height: 25px'>
+
+            </div>
+            <re-img-card type="static" feature="off">
+                <template v-slot:img>
+                    <img src="../assets/7.jpg" style='height: 355px'  alt="">
+                </template>
+                <template slot="desc">
+                    <p style="margin-top: 30px; width: 330px;"> Personal setting image heght 355px.</p>
+                </template>
+            </re-img-card>
+            <re-img-card type="static">
+                <template v-slot:img>
+                    <img src="../assets/7.jpg" style='height: 275px'  alt="">
+                </template>
+                <template slot="desc">
+                    <p style="margin-top: 30px; width: 250px;"> Personal setting image heght 275px.</p>
+                </template>
+            </re-img-card>
+            <re-img-card type="dynamic">
+                <template v-slot:img>
+                    <img src="../assets/7.jpg" style='height: 125px'  alt="">
+                </template>
+                <template slot="desc">
+                    <p style="margin-top: 30px"> 125px</p>
+                </template>
+            </re-img-card>
+        </demo-block>
+        <demo-block title='API' desc="属性说明如下：">
+          <re-table :data="tableData" type="demo"></re-table>
+        </demo-block>
     </div>
 </template>
 
@@ -61,10 +112,61 @@ export default {
   name: 'RemoInputDemo',
   components: {
     demoBlock
-  },  
+  },
+  data () {
+    return {
+      tableData: [
+        {
+          parameter: 'type',
+          description: '图片显示类型',
+          dataTypes: 'String',
+          optional: 'static / dynamic',
+          default: 'static'
+        },
+        {
+          parameter: 'feature',
+          description: '设置卡片右上角图标显示',
+          dataTypes: 'String',
+          optional: 'on / off',
+          default: 'on'
+        },
+        {
+          parameter: 'iconTop',
+          description: '第一个Icon图标',
+          dataTypes: 'String',
+          optional: '',
+          default: 'remo-share'
+        },
+        {
+          parameter: 'iconBottom',
+          description: '第二个Icon图标',
+          dataTypes: 'String',
+          optional: '',
+          default: 'remo-heart'
+        },
+        {
+          parameter: 'v-slot:img',
+          description: '显示的图片, 可设定高度',
+          dataTypes: 'slot  ',
+          optional: '',
+          default: '250px'
+        },
+        {
+          parameter: 'v-slot:desc',
+          description: '显示的描述',
+          dataTypes: 'slot',
+          optional: '',
+          default: ''
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+p{
+    width: 230px;
+    line-height: 22px;
+}
 </style>
--- packages/remo-imgcard/src/remo-imgcard.vue
+++ packages/remo-imgcard/src/remo-imgcard.vue
@@ -3,8 +3,12 @@
         <div class="imgContainer">
           <slot name="img"/>
         </div>
-        <div class="imgdesc">
+        <div class="imgdesc" :class="{'dynamic-imgdesc': type === 'dynamic'}">
           <slot name="desc"/>
+        </div>
+        <div class="feature" :class="{'dynamic-feature': type === 'dynamic', 'feature_on': feature === 'on', 'feature_off': feature === 'off' }">
+          <i class="remoi top" :class="iconTop === '' ? 'remo-share' : iconTop"    @click="iconTopClick" />
+          <i class="remoi bottom" :class="iconBottom === '' ? 'remo-heart' : iconBottom"  @click="iconBottomClick"  />
         </div>
     </div>
 </template>
@@ -13,11 +17,40 @@
 export default {
   name: 'ReImgCard',
   props: {
+    type: {
+      type: String,
+      default: 'static'
+    },
+    feature: {
+      type: String,
+      default: 'on'
+    },
+    iconTop: {
+      type: String,
+      default: ''
+    },
+    iconBottom: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    iconTopClick () {
+      this.$emit('iconTopClick')
+    },
+    iconBottomClick () {
+      this.$emit('iconBottomClick')
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+$height: 250px;
 .remo-imgcard{
   display: inline-block;
   box-sizing: border-box;
@@ -29,11 +62,26 @@ export default {
   // height: 350px;
   position: relative;
   overflow: hidden;
-  cursor: pointer;
-  // height: 300px;
+  &:last-child{
+      margin: 5px 15px;
+  }
+  &:hover{
+    .dynamic-imgdesc {
+      opacity: 1;
+    }
+    .dynamic-feature {
+       opacity: 1;
+    }
+    .feature_on {
+        opacity: 1 !important;
+    }
+    .feature_off {
+        opacity: 0  !important;
+    }
+  }
   .imgContainer img{
     // border-radius: 10px;
-    height: 255px;
+    height: $height;
   }
   .imgdesc {
     padding: 10px 15px 5px 15px;
@@ -48,6 +96,43 @@ export default {
     font-size: 15px;
     word-spacing: 1px;
     font-weight: 500;
+  }
+  .dynamic-imgdesc {
+    opacity: 0;
+    transition: 0.7s;
+  }
+  .dynamic-feature{
+      opacity: 0;
+      transition: 0.7s;
+  }
+  .feature_off {
+        opacity: 0  !important;
+    }
+  .feature{
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    padding: 13px;
+    display: flex;
+    flex-direction: column;
+    i {
+      cursor: pointer;
+      margin: 5px 0;
+      font-size: 17px;
+      overflow: hidden;
+    }
+    .top{
+       &:hover {
+        color: #409eff;
+        transform: scale(1.3);
+      }
+    }
+    .bottom{
+      &:hover {
+        color: red;
+        transform: scale(1.3);
+      }
+    }
   }
 }
 </style>
