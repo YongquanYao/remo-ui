262774140574bf162671780e100bc1b791ddf7a5
Raymond <rrraymond810@gmail.com>
2021-02-10 23:58:11 -0500

    feat message component

--- examples/components/remo-message-demo.vue
+++ examples/components/remo-message-demo.vue
@@ -1,7 +1,26 @@
 <template>
   <div>
       <demo-block header="Message 消息提醒">
-          <!-- <re-message></re-message> -->
+      </demo-block>
+      <demo-block title="基础使用">
+          <re-shadow-block width="100%" height="100%">
+            <re-message></re-message>
+          </re-shadow-block>
+      </demo-block>
+      <demo-block title="不同状态" desc="用来显示「成功、警告、消息、错误」类的操作反馈。">
+          <re-shadow-block width="100%" height="100%">
+            <div style="display: flex;">
+              <re-message text="正常消息"></re-message>
+              <re-message type="成功消息"></re-message>
+              <re-message type="警告消息"></re-message>
+              <re-message type="错误消息"></re-message>
+            </div>
+          </re-shadow-block>
+      </demo-block>
+      <demo-block title="自定义显示时间">
+          <re-shadow-block width="100%" height="100%">
+            <re-message></re-message>
+          </re-shadow-block>
       </demo-block>
   </div>
 </template>
--- packages/remo-message/src/remo-message.vue
+++ packages/remo-message/src/remo-message.vue
@@ -2,9 +2,9 @@
 <div>
   <re-button @click="sendFunction" :type="type">{{text}}
   </re-button>
-     <transition name="remo-fade" v-if="show">
-      <div class="message_detail">
-      message
+     <transition name="remo-message-fade">
+      <div class="message_detail" v-show="show">
+        <i class="remoi remo-info-circle"></i><span>{{message}}</span>
     </div>
   </transition>
 </div>
@@ -14,22 +14,35 @@
 export default {
   name: 'ReMessage',
   props: {
-    target: {
-      type: String,
-      default: 'remo-clipboard'
-    },
     text: {
       type: String,
-      default: 'Send Message'
+      default: '打开message'
+    },
+    message: {
+      type: String,
+      default: 'This is an Message'
     },
     type: {
       type: String,
       default: 'default'
+    },
+    delay: {
+      type: Number,
+      default: 4
     }
   },
   data () {
     return {
       show: false
+    }
+  },
+  watch: {
+    show: function (val) {
+      if (val) {
+        setTimeout(() => {
+          this.show = false
+        }, this.delay * 1000)
+      }
     }
   },
   methods: {
@@ -44,11 +57,38 @@ export default {
 <style lang="scss" scoped>
 /* The switch - the box around the slider */
 .message_detail{
-  position: absolute;
-  top:0;
-  width: 200px;
-  height: 30px;
-  border-radius: 6px;
-  background: rgb(96, 172, 243);
+  position: fixed;
+  left: 50%;
+  top: 25px;
+  transform: translateX(-50%);
+  padding: 20px;
+  z-index: 100;
+  transition: opacity 0.3s, transform .4s, top 0.4s;
+  overflow: hidden;
+  box-sizing: border-box;
+  width: 325px;
+  height: 40px;
+  border-radius: 4px;
+  background: rgb(125, 190, 252);
+  opacity: 1;
+  display: flex;
+  align-items: center;
+  font-size: 15px;
+  color: #fff;
+  word-spacing: 1px;
+  font-weight: 500;
+  i{
+    font-size: 20px;
+    // color: #000;
+  }
+  span{
+    margin-left: 8px;
+  }
+}
+
+.remo-message-fade-enter,
+.remo-message-fade-leave-active {
+  opacity: 0;
+  transform: translate(-50%, -100%);
 }
 </style>
